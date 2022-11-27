146b0850442fe5790ddcd0f05e994c3c10527550
dixitt5 <tilajidixit@gmail.com>
2022-11-28 00:49:26 +0530

    day 1

--- hardhat-tutorial/contracts/CryptoDevToken.sol
+++ hardhat-tutorial/contracts/CryptoDevToken.sol
@@ -7,6 +7,32 @@ import "./ICryptoDevs.sol";
 
 contract CryptoDevsToken is ERC20, Ownable {
     ICryptoDevs CryptoDevsNFT;
+    uint256 public constant tokensPerNFT = 0.001 ether;
+    uint256 public constant tokensPerNFT = 10 * 10 ** 18;
 
-    constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {}
+    mapping(uint256 => bool) public tokenIdsClaimed;
+
+    constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
+        CryptoDevsNFT = ICryptoDevs(_cryptoDevsContract);
+    }
+
+    function mint() public payable {
+        uint256 _requiredAmount = tokenPrice * 
+    }
+
+    function claim() public {
+        address sender = msg.sender;
+        uint256 balance = CryptoDevsNFT.balanceOf(sender);
+        require(balance > 0, "You don't own any Crypto Dev NFTs");
+        uint256 amount = 0;
+        for (uint i = 0; i < balance; i++) {
+            uint256 tokenId = CryptoDevsNFT.tokenOfOwnerByIndex(sender, i);
+            if (!tokenIdsClaimed[tokenId]) {
+                amount += 1;
+                tokenIdsClaimed[tokenId] = true;
+            }
+        }
+        require(amount > 0, "You have already claimed all your tokens");
+        _mint(msg.sender, amount * tokensPerNFT);
+    }
 }
