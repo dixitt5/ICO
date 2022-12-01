e0782dcb4b6b095587d2db318d77eaafc62678a6
dixitt5 <tilajidixit@gmail.com>
2022-12-01 15:21:04 +0530

    contract created and deployed

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config({ path: ".env" });
+
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
--- hardhat-tutorial/scripts/deploy.js
+++ hardhat-tutorial/scripts/deploy.js
@@ -1,12 +1,14 @@
 const { ethers } = require("hardhat");
 require("dotenv").config({ path: ".env" });
-
+const { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
 async function main() {
   const crypoDevsTokenContract = await ethers.getContractFactory(
     "CryptoDevToken"
   );
 
-  const deployCryptoDevTokenContract = await crypoDevsTokenContract.deploy();
+  const deployCryptoDevTokenContract = await crypoDevsTokenContract.deploy(
+    CRYPTO_DEVS_NFT_CONTRACT_ADDRESS
+  );
 
   console.log(
     "CryptDev Token Contract address:",
