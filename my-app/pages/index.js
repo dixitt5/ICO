ba8b02de6f00dc1ae0ff7c6b746aaaba8e934421
dixitt5 <tilajidixit@gmail.com>
2022-12-01 16:52:29 +0530

    making the app

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -2,11 +2,37 @@ import Head from "next/head";
 import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
-import Web3Modal from "web3modal";
+import Web3Modal, { providers } from "web3modal";
 
 export default function Home() {
   const [walletConnected, setWalletConnected] = useState(false);
   const web3ModalRef = useRef();
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    //provider is metamask
+    const web3Provider = new providers.Web3Provider(provider);
+    const { chainId } = await web3Provider.getNetwork();
+
+    if (chainId !== 5) {
+      window.alert("change the network to Goerli");
+      throw new Error("Change network to goerli");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   useEffect(() => {
     if (!walletConnected) {
@@ -15,6 +41,7 @@ export default function Home() {
         providerOptions: {},
         disableInjectedProvider: false,
       });
+      connectWallet();
     }
   }, []);
 
